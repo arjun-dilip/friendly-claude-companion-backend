399a804a3a61eece432fc9ce176b79cb0d7c85b6
Arjun <57611922+arjun-dilip@users.noreply.github.com>
2025-03-27 15:36:27 +1100

    hook up to claude with data from req

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -7,16 +8,19 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+const jsonParser = bodyParser.json();
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/api/claude", async (req, res) => {
+app.post("/api/claude", jsonParser, async (req, res) => {
   const apiKey = process.env.ANTHROPIC_API_KEY;
 
   if (apiKey) {
+    console.log(`----> User prompt: ${JSON.stringify(req.body)}`);
     const response = await fetch("https://api.anthropic.com/v1/messages", {
       method: "POST",
       headers: {
@@ -24,24 +28,18 @@ app.post("/api/claude", async (req, res) => {
         "x-api-key": apiKey,
         "anthropic-version": "2023-06-01",
       },
-      body: JSON.stringify({
-        model: "claude-3-5-sonnet-20240620",
-        max_tokens: 300,
-        messages: [
-          {
-            role: "user",
-            content: "Hello, can you introduce yourself?",
-          },
-        ],
-      }),
+      body: JSON.stringify(req.body),
     });
 
     const resp = await response.json();
 
+    console.log(`----> claude response: ${JSON.stringify(resp)}`);
     res.send(JSON.stringify(resp));
   } else {
     res.send("No API key found");
   }
+
+  // res.send("debug mode, son");
 });
 
 app.listen(port, () => {
